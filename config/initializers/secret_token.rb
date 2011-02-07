6c946835f18bde81d6f4355dd8a54c377ec8dc54
JT Archie <jtarchie@gmail.com>
2011-02-06 19:19:20 -0800

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postponeit::Application.config.secret_token = 'f5f5686223647876ecbd7fd498b5b099fce9de4eb119457ed970a07b3c7ab79a83a3fdb364cec2688ae8dfcc6bbfc7a2de91d2774ba12e846110fd01e9f08620'
