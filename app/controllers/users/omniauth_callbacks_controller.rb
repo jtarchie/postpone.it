79707c7eadfff409ed99df7d182c25dec7a52b8b
JT Archie <jtarchie@gmail.com>
2011-02-23 19:05:03 -0800

    facebook now records the oauth token

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def method_missing(method, *args)
-    raise "Unknown Provider Method: #{method}" unless method.to_s.downcase =~ /facebook/
+  def facebook
     omniauth = request.env['omniauth.auth']
     if current_user
-      current_user.authorizations.create(:provider => omniauth['provider'], :uid => omniauth['uid'])
-      redirect_to accounts_path, :notice => "Successfully authorized with #{method.to_s.humanize}"
+      authorization = current_user.authorizations.where(:provider => omniauth['provider'], :uid => omniauth['uid']).create
+      authorization.update_attributes :token => omniauth['credentials']['token']
+      redirect_to accounts_path, :notice => "Successfully authorized with Facebook"
     end
   end
 end
